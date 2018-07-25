bf890355a73bca072fe4d23e396b91a998c20a73
jonathandannel <usixjad@gmail.com>
2018-07-25 20:59:36 +0000

    added socket connection on app mount

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,14 @@ import Main from './Main.jsx';
 import NavBar from './Components/NavBar.jsx';
 
 class App extends Component {
+
+  componentDidMount() {
+    this.socket = new WebSocket("ws://0.0.0.0:3001");
+    this.socket.onopen = (e) => {
+      console.log('==> Socket connection started!')
+    }
+  }
+
   render() {
     return (
       <div>
