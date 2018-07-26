1766ab5b6aed23191f8851d7ebb2b8e5bf0e6a7c
Farid Manafov <faridbardium@gmail.com>
2018-07-26 14:54:31 -0400

    fixed div closing

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -7,11 +7,11 @@ export default class Login extends Component {
         <form action="/login" method="post">
           <div>
             <label for="username">Username: </label>
-            <input type="text" id="username" name="user-name">
+            <input type="text" id="username" name="user-name"/>
           </div>
           <div>
             <label for="password">Password: </label>
-            <input type="text" id="password" name="password">
+            <input type="text" id="password" name="password"/>
           </div>
           <div class="button">
             <button type="submit">Login</button>
