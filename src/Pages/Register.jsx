dccc716e9af7fc3bba1d57473355ec454c26bb12
suwignyo <gsuwignyo@gmail.com>
2018-08-01 22:02:42 +0000

    fixed register bug

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -37,29 +37,43 @@ export default class Register extends Component {
     }
     return (
       <div>
-        <form action="/register" method="post" onSubmit={this.handleFormSubmit}>
-          <div>
-            <label htmlFor="username">Username: </label>
-            <input
-            className="username"
-            name="username"
-            type="text"
-            onChange={this.handleChange}
-            />
-            <label htmlFor="password">Password: </label>
-            <input
-            className="password"
-            name="password"
-            type="password"
-            onChange={this.handleChange}
-            />
-            <button
-            className="submit"
-            type="submit"
-            value="SUBMIT"
-            >Register</button>
-          </div>
-        </form>
+        <Button onClick={this.handleClickOpen}>Register</Button>
+          <Dialog
+            open={this.state.open}
+            onClose={this.handleClose}
+            aria-labelledby="form-dialog-title"
+          >
+            <DialogTitle id="form-dialog-title">Register</DialogTitle>
+            <DialogContent>
+              <TextField
+                autoFocus
+                margin="dense"
+                label="Username"
+                type="text"
+                fullWidth
+                value={this.state.username}
+                onChange={this.handleUser}
+              />
+              <TextField
+                autoFocus
+                margin="dense"
+                label="Password"
+                type="password"
+                fullWidth
+                value={this.state.password}
+                onChange={this.handlePass}
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={this.handleClose} color="primary">
+                Cancel
+              </Button>
+              <Button         
+                      onClick={this.handleFormSubmit} color="primary">
+                Register
+              </Button>
+            </DialogActions>
+          </Dialog>
       </div>
     )
   }
@@ -95,79 +109,3 @@ export default class Register extends Component {
       })
   }
 }
-
-
-
-
-{/* <Button onClick={this.handleClickOpen}>Register</Button>
-<Dialog
-  open={this.state.open}
-  onClose={this.handleClose}
-  aria-labelledby="form-dialog-title"
->
-  <DialogTitle id="form-dialog-title">Register</DialogTitle>
-  <DialogContent>
-    <TextField
-      autoFocus
-      margin="dense"
-      label="Username"
-      type="text"
-      fullWidth
-      onChange={this.handleChange}
-    />
-    <TextField
-      autoFocus
-      margin="dense"
-      label="Password"
-      type="password"
-      fullWidth
-      onChange={this.handleChange}
-    />
-  </DialogContent>
-  <DialogActions>
-    <Button onClick={this.handleClose} color="primary">
-      Cancel
-    </Button>
-    <Button
-
-            onClick={this.handleFormSubmit} color="primary">
-      Register
-    </Button>
-  </DialogActions>
-</Dialog> */}
-
-
-
-        {/* <div>
-          <label for="username">Username: </label>
-          <input
-          className="username"
-          name="username"
-          type="text"
-          onChange={this.handleChange}
-          />
-          <label for="password">Password: </label>
-          <input
-          className="password"
-          name="password"
-          type="password"
-          onChange={this.handleChange}
-          />
-          <button
-          className="submit"
-          type="submit"
-          value="SUBMIT"
-          >Register</button>
-        // </div> */}
-
-        // getInitialUsername = () => {
-        //   return {
-        //     username: ''
-        //   }
-        // }
-
-        // getInitialPassword = () => {
-        //   return {
-        //     password: ''
-        //   }
-        // }
