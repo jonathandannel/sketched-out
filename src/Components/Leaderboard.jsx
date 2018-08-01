aa2d80a42adeb480635e6f161cf8f7bc5cfc9611
Farid <faridbardium@gmail.com>
2018-08-01 20:08:57 +0000

    leaderboard works without DB, need to migrate to the DB database for leaderboards

--- src/Components/Leaderboard.jsx
+++ src/Components/Leaderboard.jsx
@@ -1,28 +1,59 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
-const leaderboardScores = () => {
-  users.map(user => {
+const arrayOfUsers = [
+  {
+    username: "farid",
+    totalPoints: 12
+  },
+  {
+    username: "anar",
+    totalPoints: 16
+  },
+  {
+    username: "idana",
+    totalPoints: 22
+  },
+  {
+    username: "mom",
+    totalPoints: 9
+  }
+];
+console.log(arrayOfUsers, "array of users");
+
+arrayOfUsers.sort((a, b) => {
+  return b.totalPoints - a.totalPoints;
+});
+
+console.log(arrayOfUsers, "after sort");
+
+const LeaderboardScores = () => {
+  const displayTopUsers = arrayOfUsers.map(user => (
     <tr>
-      <td>user.username</td>
-      <td>user.score</td>
+      <td>{user.username}</td>
+      <td>{user.totalPoints}</td>
     </tr>
-  })
-}
-
+  ));
+  return <tbody>{displayTopUsers}</tbody>;
+};
+// console.log(leaderboardScores)
 export default class Leaderboard extends Component {
   render() {
     return (
       <div>
         <table>
-        <thead colspan="2">High Scores</thead>
-          <th>User</th>
-          <th>Score</th>
-        <tbody>
-          {leaderboardScores}
-        </tbody>
-      </table>
+          <thead>
+            <tr>
+              <th>User</th>
+
+              <th>Score</th>
+            </tr>
+          </thead>
+          {/* <tbody> */}
+          {/* {leaderboardScores} */}
+          <LeaderboardScores />
+          {/* </tbody> */}
+        </table>
       </div>
-    )
+    );
   }
 }
-
