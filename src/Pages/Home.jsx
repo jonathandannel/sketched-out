a8a3442f3207873f8407af691343c8f5b32f5a0f
jonathandannel <usixjad@gmail.com>
2018-08-08 17:31:40 +0000

    asdasd

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,6 +3,7 @@ import Button    from '@material-ui/core/Button';
 import { Link } from 'react-router-dom'
 import Leaderboard from '../Components/Leaderboard.jsx'
 import SimpleModalWrapped from '../Components/Rules-modal.jsx';
+import Card from '@material-ui/core/Card';
 
 
 export default class Home extends Component {
@@ -11,7 +12,7 @@ export default class Home extends Component {
   render() {
     return (
       <div className="homepage-bg">
-        
+
         <div className="join-button-div">
           <Button className='join-button'>
             <Link className='join-text' to="room">JOIN THE GAME <img id="join-btn-img" src="./styles/inkpen.png" alt="inkpen"/></Link>
@@ -26,5 +27,3 @@ export default class Home extends Component {
     )
   }
 }
-
-
